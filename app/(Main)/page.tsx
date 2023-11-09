2050af107b284326c29bcab699d24da9e06e5d63
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-09 12:54:55 +0600

    code commit

---
+++ app/(Main)/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from 'react'
+import Header from '@/components/Header';
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default page
+
