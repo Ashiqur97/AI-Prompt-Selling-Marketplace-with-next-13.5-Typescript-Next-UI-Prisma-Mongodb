69a7eb09e60944c2e30452478400977efef8fcbd
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-09 19:05:43 +0600

    code commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Navigation from "./Navigation";
 import {AiOutlineSearch} from 'react-icons/ai';
 import {CgProfile} from 'react-icons/cg'
+import {FaBars} from 'react-icons/fa'
 
 type Props = {
     activeItem:number;
@@ -10,6 +11,7 @@ type Props = {
 
 const Header = (activeItem: Props) => {
     const [active, setactive] = useState(false);
+    const [open,setOpen] = useState(false);
 
     if (typeof window !== "undefined") {
         window.addEventListener("scroll", () => {
@@ -19,6 +21,13 @@ const Header = (activeItem: Props) => {
             setactive(false);
           }
         });
+      }
+
+      const handleClose = (e:React.MouseEvent) => {
+        const target = e.target as HTMLElement;
+        if(target.id === 'screen') {
+            setOpen(!open);
+        }
       }
   return (
     <div className={`w-full p-5 border-b min-h-[60px] border-b-[#ffffff32] transition-opacity ${
@@ -45,6 +54,40 @@ const Header = (activeItem: Props) => {
                     <CgProfile className="text-[25px] cursor-pointer"/>
                 </Link>
             </div>
+        </div>
+
+        <div className='w-full md:hidden flex items-center justify-between'>
+            <div>
+            <Link href="/">
+            <h1>
+              <Link href={"/"}>
+                <h1 className="font-Inter text-3xl cursor-pointer">
+                  <span className="text-[#64ff4c]">Bec</span>odemy
+                </h1>
+              </Link>
+            </h1>
+          </Link>
+            </div>
+            <div>
+            <FaBars
+            className="text-2xl cursor-pointer"
+            onClick={() => setOpen(!open)}
+          />
+            </div>
+            {
+                open && (
+                    <div className='fixed md:hidden w-full h-screen top-0 left-0 z-[99999] bg-[unset]'
+                    onClick={handleClose}
+                    id="screen"
+                    >
+                        <div className="fixed bg-black h-screen top-0 right-0 w-[60%] z-[9999]">
+                                <div className='mt-20 p-5'>
+                                    <Navigation activeItem={activeItem} />
+                                </div>  
+                            </div>
+                        </div>
+                )
+            }
         </div>
     </div>
   )
