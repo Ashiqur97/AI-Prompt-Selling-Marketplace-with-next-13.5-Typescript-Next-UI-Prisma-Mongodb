67fd9c4a342cd9ce9c28bd1a09e215f47bb068ec
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-10 00:48:44 +0600

    code commit change

--- components/Route/Hero.tsx
+++ components/Route/Hero.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import Image from "next/image";
 import line from "@/public/Assets/line.png";
+import MarQuee from "react-fast-marquee";
 
 type Props = {}
 
 const Hero = (props: Props) => {
+
+    const rowOneImages = [
+        {
+            url: "https://pixner.net/aikeu/assets/images/banner/large-slider/one.png",
+          },
+          {
+            url: "https://pixner.net/aikeu/assets/images/banner/large-slider/two.png",
+          },
+          {
+            url: "https://pixner.net/aikeu/assets/images/banner/large-slider/three.png",
+          },
+          {
+            url: "https://pixner.net/aikeu/assets/images/banner/large-slider/four.png",
+          },
+          {
+            url: "https://pixner.net/aikeu/assets/images/banner/large-slider/five.png",
+          },
+    ];
+
+    const rowTwoImages = [
+        {
+          url: "https://pixner.net/aikeu/assets/images/banner/small-slider/one.png",
+        },
+        {
+          url: "https://pixner.net/aikeu/assets/images/banner/small-slider/two.png",
+        },
+        {
+          url: "https://pixner.net/aikeu/assets/images/banner/small-slider/three.png",
+        },
+        {
+          url: "https://pixner.net/aikeu/assets/images/banner/small-slider/four.png",
+        },
+        {
+          url: "https://pixner.net/aikeu/assets/images/banner/small-slider/five.png",
+        },
+      ];
+
   return (
     <div className='w-full md:min-h-screen flex items-center justify-center'>
           <div>
@@ -20,6 +58,35 @@ const Hero = (props: Props) => {
             width={2000}
             height={2}
           />
+        </div>
+        <div className='w-[100vw] mb-5 md:mb-20 relative'>
+            <div className='rotate-[-4deg] mt-10 md:mt-[6.5rem]'>
+                <MarQuee>
+                {rowOneImages.map((i, index) => (
+                <Image
+                  src={i.url}
+                  key={index}
+                  alt=""
+                  className="md:m-4 w-[200px] m-2 md:w-[500px] rounded-[20px]"
+                  width={500}
+                  height={300}
+                />
+              ))}
+                </MarQuee>
+
+                <MarQuee>
+                {rowTwoImages.map((i, index) => (
+                <Image
+                  src={i.url}
+                  key={index}
+                  alt=""
+                  className="md:m-4 w-[200px] m-2 md:w-[500px] rounded-[20px]"
+                  width={500}
+                  height={300}
+                />
+              ))}
+                </MarQuee>
+            </div>
         </div>
         </div>
     </div>
