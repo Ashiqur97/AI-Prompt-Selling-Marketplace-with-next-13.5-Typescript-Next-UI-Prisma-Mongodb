37553037513b51652a05a4c887a1cca3cc36d60f
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-10 00:05:58 +0600

    code commit change

--- components/Route/Hero.tsx
+++ components/Route/Hero.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import Image from "next/image";
+import line from "@/public/Assets/line.png";
 
 type Props = {}
 
 const Hero = (props: Props) => {
   return (
     <div className='w-full md:min-h-screen flex items-center justify-center'>
-             <h1 className="font-Monserrat text-4xl py-5 xl:text-7xl 2xl:text-8xl font-[700] text-center xl:leading-[80px] 2xl:leading-[100px] sm:mt-20">
+          <div>
+        <h1 className="font-Monserrat text-4xl py-5 xl:text-7xl 2xl:text-8xl font-[700] text-center xl:leading-[80px] 2xl:leading-[100px] sm:mt-20">
           Make <span className="text-[#64FF4B]">Ai Image</span> <br /> With Your{" "}
           <br /> Imagination
         </h1>
+        <div className="md:mt-5">
+          <Image
+            src={line}
+            alt=""
+            className="absolute hidden md:block"
+            width={2000}
+            height={2}
+          />
+        </div>
+        </div>
     </div>
   )
 }
