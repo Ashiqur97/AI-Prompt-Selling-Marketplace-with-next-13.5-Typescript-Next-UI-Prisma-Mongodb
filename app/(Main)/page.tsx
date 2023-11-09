09c59f4b38d14b6a2ddf6627989fe68c1e83bd8b
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-09 23:51:47 +0600

    code commit change

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -1,13 +1,27 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import Header from '@/components/Header';
+import Hero from '@/components/Route/Hero';
+
 
 type Props = {}
 
 const Page = (props: Props) => {
+  const [isMounted,setIsMounted] = useState(false);
+  useEffect(() => {
+    if(!isMounted){
+      setIsMounted(true);
+    }
+  },[isMounted]);
+
+  if(!isMounted){
+    return null;
+  }
+
   return (
     <div>
       <Header activeItem={0}/>
+      <Hero />
     </div>
   )
 }
