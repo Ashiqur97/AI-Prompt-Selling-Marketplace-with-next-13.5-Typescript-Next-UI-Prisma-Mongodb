eff8445c9ae1e536fd015e5c12380a0d55ce0b54
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-10 17:03:17 +0600

    change

--- components/Prompts/PromptCard.tsx
+++ components/Prompts/PromptCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { styles } from "@/utils/styles";
 import {Card} from '@nextui-org/react'
 import Image from 'next/image'
 
@@ -15,6 +16,24 @@ const PromptCard = (props: Props) => {
                    width={300}
                    height={300}
             />
+            <div className='absolute bottom-2 left-2'>
+                <div className='w-max bg-black hover:bg-[#16252] duration-300 transition-opacity hover:text-black text-white p-[10px] items-center flex rounded-xl'>
+                <Image
+                src="https://pixner.net/aikeu/assets/images/category/chat.png"
+                width={25}
+                height={25}
+                alt=""
+              />
+              <span className={`${styles.label} pl-2 text-white`}>Chatgpt</span>
+                </div>
+            </div>
+        </div>
+
+        <div className='w-full flex justify-between py-2'>
+            <h3 className={`${styles.label} text[18px] text-white`}>
+                Animal Prompts
+            </h3>
+            <p className={`${styles.paragraph}`}>$25.00</p>
         </div>
     </Card>
   )
