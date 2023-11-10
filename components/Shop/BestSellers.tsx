7d773654c4e0687da691bb1134e51493e737f963
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-10 18:16:21 +0600

    code commit

---
+++ components/Shop/BestSellers.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {styles} from "@/utils/styles"
+import SellerCard from "./SellerCard"
+
+type Props = {}
+
+const BestSellers = (props: Props) => {
+  return (
+    <div className='mb-10 cursor-pointer'>
+        <h1 className={`${styles.heading} p-2 font-Monserrat mb-5`}>
+            Top Sellers
+        </h1>
+        <div className='flex flex-wrap'>
+            <SellerCard />
+        </div>
+    </div>
+  )
+}
+
+export default BestSellers
---
+++ components/Shop/SellerCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SellerCard = (props: Props) => {
+  return (
+    <div>SellerCard</div>
+  )
+}
+
+export default SellerCard
