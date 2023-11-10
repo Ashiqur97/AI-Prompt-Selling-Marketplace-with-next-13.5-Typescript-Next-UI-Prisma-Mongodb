978e2f5dab27b9acb2d770facc6eb1f427b4afd6
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-10 12:13:56 +0600

    code change

--- components/Route/About.tsx
+++ components/Route/About.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {styles} from '@/utils/styles'
 import {Chip} from '@nextui-org/react'
 
 type Props = {}
@@ -7,8 +8,8 @@ const About = (props: Props) => {
   return (
     <div className='w-full relative grid md:grid-cols-2 md:py-8'>
         <div className='col-span-1 w-full md:w-[60%] md:mt-5 px-5 md:px-[unset]'>   
-            <Chip className={''}>
-                
+            <Chip className={`${styles.button} mb-[30px] h-[37px] bg-[#122211f]`}>
+                AI Image
             </Chip>
         </div>
     </div>
