37aaea4d3321276e00a06be727c3c4ec7627f7ee
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-10 12:39:58 +0600

    code commit

--- components/Route/About.tsx
+++ components/Route/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {styles} from '@/utils/styles'
-import {Chip} from '@nextui-org/react'
+import {Button,Chip} from '@nextui-org/react'
+import Image from "next/image";
 
 type Props = {}
 
@@ -11,6 +12,28 @@ const About = (props: Props) => {
             <Chip className={`${styles.button} mb-[30px] h-[37px] bg-[#122211f]`}>
                 AI Image
             </Chip>
+            <h5 className={`${styles.heading} mb-5 !leading-[50px]`}>
+                Crafting Tomorrow&apos;s Images With Artificial Intelligence
+            </h5>
+            <p className={`${styles.paragraph} pb-5`}>
+            AI image generation tools have emerged as powerful resources in the
+          realm of digital art and design. These cutting-edge tools leverage
+          advanced.
+            </p>
+            <Button
+          className={`${styles.button} bg-[#2551b0] font-[500] h-[45px]`}          
+        >
+          Visit Shop
+        </Button>
+        </div>
+        <div className='col-span-1 my-10 md:mt--[unset]'>
+        <Image
+          src={"https://pixner.net/aikeu/assets/images/craft-thumb.png"}
+          alt=""
+          width={600}
+          height={600}
+          priority
+        />
         </div>
     </div>
   )
