288cfca227abd83680d77dc4f2580b362d9b3fa6
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-12 01:16:20 +0600

    fixe the code

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
 
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
+    <div className='mt-8'>
+        <div className="w-full mb-5 flex justify-between items-center">
+            <Link href={"/"}>
+            <h1 className="font-Inter text-3xl cursor-pointer">
+              <span className="text-[#64ff4c]">Bec</span>odemy
+            </h1>
+            </Link>
+        </div>
+    </div>
   )
 }
 
