42e9fef90487eb084e1cc5f0345b529135739c16
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-12 01:24:27 +0600

    change the footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,20 +1,56 @@
 import React from 'react'
 import Link from 'next/link'
+import { styles } from "@/utils/styles";
 
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div className='mt-8'>
-        <div className="w-full mb-5 flex justify-between items-center">
-            <Link href={"/"}>
-            <h1 className="font-Inter text-3xl cursor-pointer">
-              <span className="text-[#64ff4c]">Bec</span>odemy
-            </h1>
+    <div className="mt-8">
+    <div className="w-full mb-5 flex justify-between items-center">
+      <div>
+        <Link href={"/"}>
+          <h1 className="font-Inter text-3xl cursor-pointer">
+            <span className="text-[#64ff4c]">Bec</span>odemy
+          </h1>
+        </Link>
+      </div>
+      <div>
+        <ul className="flex items-center flex-wrap">
+          <li>
+            <Link
+              href="/"
+              className={`${styles.label} hover:text-[#64ff4b] duration-200 transition px-4`}
+            >
+              Home
             </Link>
-        </div>
+          </li>
+          <li>
+            <Link
+              href="/marketplace"
+              className={`${styles.label} hover:text-[#64ff4b] duration-200 transition px-4`}
+            >
+              MarketPlace
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/contact-us"
+              className={`${styles.label} hover:text-[#64ff4b] duration-200 transition px-4`}
+            >
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+    <p className={`${styles.paragraph} text-center`}>
+      Copyright © 2023 Becodemy . All Rights Reserved
+    </p>
+    <br />
+    <br />
+  </div>
+);
+};
 
-export default Footer
+export default Footer;
