bdac78d8c226ab255ecdfb8f12ea383581772839
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-13 01:24:26 +0600

    code fixed

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -4,10 +4,10 @@ import Navigation from "./Navigation";
 import {AiOutlineSearch} from 'react-icons/ai';
 import {CgProfile} from 'react-icons/cg'
 import {FaBars} from 'react-icons/fa'
-import { UserButton } from '@clerk/nextjs';
 import {User} from "@clerk/nextjs/server";
 import DropDown from "./DropDown";
-
+import { UserProfile } from "@clerk/nextjs";
+import { RxCross1 } from "react-icons/rx";
 
 type Props = {
     activeItem:number;
@@ -80,6 +80,21 @@ const Header = (user,activeItem: Props) => {
                   }
             </div>
         </div>
+
+        {
+          activeProfile && (
+            <div className="w-full fixed h-screen overflow-hidden flex justify-center items-center top-0 left-0 bg-[#00000068] z-[9999]">
+            <div className="w-min relative h-[90vh] overflow-y-scroll bg-white rounded-xl shadow">
+              <UserProfile />
+
+              <RxCross1
+              className="absolute text-black text-2xl top-10 right-10 cursor-pointer"
+              onClick={handleProfile}
+            />
+                </div>
+                  </div>
+          )
+        }
 
         <div className='w-full md:hidden flex items-center justify-between'>
             <div>
