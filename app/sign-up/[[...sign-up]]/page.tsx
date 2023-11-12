0fbde7d8d80fa431db385993df394b16fec1865e
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-12 19:16:49 +0600

    code fixed

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+        <SignUp />
+    </div>
+  
+  );
 }
