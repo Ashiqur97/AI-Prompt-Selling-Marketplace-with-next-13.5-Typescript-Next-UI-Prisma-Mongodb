e736325831a46b294c7f566d1f60d869a68c7d59
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-17 00:28:15 +0600

    code fixed

--- components/Shop/UploadPrompt.tsx
+++ components/Shop/UploadPrompt.tsx
@@ -1,60 +1,84 @@
-import React,{useState} from 'react'
-import {styles} from "@/utils/styles"
+import { styles } from "@/utils/styles";
 import { useAuth } from "@clerk/nextjs";
 import {
-    Button,
-    Input,
-    Select,
-    SelectItem,
-    Selection,
-    Textarea,
-  } from "@nextui-org/react";
+  Button,
+  Input,
+  Select,
+  SelectItem,
+  Selection,
+  Textarea,
+} from "@nextui-org/react";
+import { IoDocumentAttachOutline } from "react-icons/io5";
+import Image from "next/image";
+import React, { ChangeEvent, DragEvent, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { redirect } from "next/navigation";
 
-type Props = {}
+type Props = {};
 
 type PromptData = {
-    name: string;
-    shortDescription: string;
-    description: string;
-    images: string[];
-    attachments: string[];
-    estimatedPrice: string;
-    price: string;
-    tags: string;
-  };
-  
-  const categorieItem = [
-    {
-      title: "Chatgpt",
-    },
-    {
-      title: "Midjourney",
-    },
-    {
-      title: "Bard",
-    },
-    {
-      title: "Dalle",
-    },
-  ];
+  name: string;
+  shortDescription: string;
+  description: string;
+  images: string[];
+  attachments: string[];
+  estimatedPrice: string;
+  price: string;
+  tags: string;
+};
+
+const categorieItem = [
+  {
+    title: "Chatgpt",
+  },
+  {
+    title: "Midjourney",
+  },
+  {
+    title: "Bard",
+  },
+  {
+    title: "Dalle",
+  },
+];
 
 const UploadPrompt = (props: Props) => {
-    const [promptData,setPromptData] = useState({
-        name: "",
-        shortDescription: "",
-        description: "",
-        images: [],
-        attachments: [],
-        estimatedPrice: "",
-        price: "",
-        tags: "",
-    });
+  const [promptData, setPromptData] = useState<PromptData>({
+    name: "",
+    shortDescription: "",
+    description: "",
+    images: [],
+    attachments: [],
+    estimatedPrice: "",
+    price: "",
+    tags: "",
+  });
+  const [dragging, setDragging] = useState<Boolean>(false);
+  const { userId } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [category, setCategory] = useState<Selection>(new Set([]));
 
-    const [dragging, setDragging] = useState<Boolean>(false);
-    const { userId } = useAuth();
-    const [isLoading, setIsLoading] = useState(false);
-    const [category, setCategory] = useState<Selection>(new Set([]));
+  const handleImageFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const files = Array.from(e.target.files);
 
+      files.forEach((file) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          if (reader.readyState === 2) {
+            setPromptData((prevData) => ({
+              ...prevData,
+              images: [...prevData.images, reader.result as string],
+            }));
+          }
+        };
+        reader.readAsDataURL(file);
+      });
+    }
+  };
+
+  
   return (
     <div>
         <h1 className={`${styles.heading} text-center py-5`}>
