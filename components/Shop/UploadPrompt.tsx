d5d3296459bead1b115e0da27918461d5a6c8c4c
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-17 00:16:12 +0600

    code commit

--- components/Shop/UploadPrompt.tsx
+++ components/Shop/UploadPrompt.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {styles} from "@/utils/styles"
+
 
 type Props = {}
 
+type PromptData = {
+    name: string;
+    shortDescription: string;
+    description: string;
+    images: string[];
+    attachments: string[];
+    estimatedPrice: string;
+    price: string;
+    tags: string;
+  };
+  
+  const categorieItem = [
+    {
+      title: "Chatgpt",
+    },
+    {
+      title: "Midjourney",
+    },
+    {
+      title: "Bard",
+    },
+    {
+      title: "Dalle",
+    },
+  ];
+
 const UploadPrompt = (props: Props) => {
+    const [promptData,setPromptData] = useState({
+        name: "",
+        shortDescription: "",
+        description: "",
+        images: [],
+        attachments: [],
+        estimatedPrice: "",
+        price: "",
+        tags: "",
+    });
+    
   return (
     <div>
         <h1 className={`${styles.heading} text-center py-5`}>
