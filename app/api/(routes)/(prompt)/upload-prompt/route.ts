23900a5486979d55408a4739b1862a791fda58c0
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-17 17:06:05 +0600

    code coomit changes

--- app/api/(routes)/(prompt)/upload-prompt/route.ts
+++ app/api/(routes)/(prompt)/upload-prompt/route.ts
@@ -1,3 +1,30 @@
 import prisma from "@/lib/prismaDb";
 import cloudinary from "@/lib/cloudinary";
 
+import {User,currentUser} from "@clerk/nextjs/server";
+import { NextRequest,NextResponse } from "next/server";
+
+export async function POST(req:NextRequest) {
+    try {
+        const data = await req.json();
+        const user:User | null = await currentUser();
+        
+        if(data.images && data.images.length > 0) {
+            const validImages = data.images.filter((image:string) => image!==undefined);
+
+            const uploadImages = await Promise.all(
+                validImages.map(async(image:string) => {
+                    const result = await cloudinary.uploader.upload(image);
+                    return {
+                        create:{
+                            public_id:result.public_id,
+                            url:result.secure_url,
+                        }
+                    }
+                })
+            )
+        }
+    } catch (error) {
+
+    }
+}
